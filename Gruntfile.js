c6370dbf2aa8464e9d1fba1ea2429fb5a4b7cd49
Carey Hinoki <carey.r.hinoki.-nd@disney.com>
2016-06-09 15:36:23 -0700

    feat(grunt): Add `grunt-bump` task.

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+module.exports = function Gruntfile(grunt) {
+  // load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // time how long tasks take. can help when optimizing build times
+  require('time-grunt')(grunt);
+
+  grunt.loadTasks('grunt');
+};
